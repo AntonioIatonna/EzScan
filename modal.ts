723ed78fc499593edfee733e6660407d35ebd1a6
Gabriel Rueda <ruedag@uwindsor.ca>
2023-10-08 20:15:50 -0400

    Able to Read PDF File

--- modal.ts
+++ modal.ts
@@ -1,4 +1,10 @@
 import { App, FuzzySuggestModal, Modal, Notice, TFile } from "obsidian";
+import { readFile, readFileSync } from 'fs';
+
+
+import * as fs from 'fs';
+import * as pdfjs from "pdfjs-dist";
+import * as worker from "pdfjs-dist/build/pdf.worker.entry.js";
 
 
 export class PDFtoTextModal extends FuzzySuggestModal<TFile> {
@@ -19,7 +25,34 @@ export class PDFtoTextModal extends FuzzySuggestModal<TFile> {
     return file.name;
   }
 
-  onChooseItem(file: TFile, evt: MouseEvent | KeyboardEvent) {
-    new Notice(`Selected ${file.extension}`);
+  async getPdfText(data: Buffer) {
+      let doc = await pdfjs.getDocument({data}).promise;
+
+      const x = (await doc.getPage(2)).getTextContent()
+      console.log(x)
+
+      let pageTexts = Array.from({length: doc.numPages}, async (v,i) => {
+          return (await (await doc.getPage(i+1)).getTextContent()).items.map(token => token.str).join('');
+      });
+      return (await Promise.all(pageTexts)).join('\n');
   }
-}
+
+
+  async onChooseItem(file: TFile, evt: MouseEvent | KeyboardEvent) {
+		// pdfjs.GlobalWorkerOptions.workerSrc = worker;
+
+    const basePath = (this.app.vault.adapter as any).basePath
+
+
+    pdfjs.GlobalWorkerOptions.workerSrc = worker;
+
+    const filePath = basePath + "/" + file.path;
+
+    const doc = await fs.readFileSync(filePath);
+
+    const res = await this.getPdfText(doc);
+
+    console.log(res);
+    }
+    
+  }
